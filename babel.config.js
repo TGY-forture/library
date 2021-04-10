ba56c423987f0b2e122196b84ab17f4b1296cf10
TGY-forture <1981472184@qq.com>
2021-04-10 22:25:49 +0800

    babel config

---
+++ babel.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  presets: [
+    "@babel/preset-env"
+  ],
+  plugins: [
+    [
+      'import',
+      {
+        libraryName: 'vant',
+        libraryDirectory: 'es',
+        style: true,
+      },
+      'vant',
+    ],
+    [
+      "import",
+      {
+        libraryName: "ant-design-vue",
+        libraryDirectory: "es",
+        style: "css"
+      }
+    ]
+  ]
+}
