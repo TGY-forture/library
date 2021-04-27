9764ac2c94dce5c34449f3a0ea3d0f62c766baf8
TGY-forture <1981472184@qq.com>
2021-04-27 22:01:35 +0800

    config mysql connect

--- server/util/mysql.js
+++ server/util/mysql.js
@@ -1,1 +1,20 @@
 const { createConnection } = require('mysql');
+
+const createConn = () => {
+  let connection = createConnection({
+    host: 'rm-2zeim991vtb92p54wao.mysql.rds.aliyuncs.com',
+    port: '3306',
+    user: 'tgy',
+    password: 'tgy12345',
+    database: 'library',
+    multipleStatements: true
+  })
+  connection.connect(function (err) {
+    if (err) {
+      throw err;
+    }
+  })
+  return connection;
+}
+
+module.exports = createConn;
