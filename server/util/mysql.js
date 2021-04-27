486b9d0494b43e709dd0413f527c564671bba437
TGY-forture <1981472184@qq.com>
2021-04-27 22:12:15 +0800

    delete

--- server/util/mysql.js
+++ server/util/mysql.js
@@ -1,14 +1,7 @@
 const { createConnection } = require('mysql');
 
 const createConn = () => {
-  let connection = createConnection({
-    host: 'rm-2zeim991vtb92p54wao.mysql.rds.aliyuncs.com',
-    port: '3306',
-    user: 'tgy',
-    password: 'tgy12345',
-    database: 'library',
-    multipleStatements: true
-  })
+
   connection.connect(function (err) {
     if (err) {
       throw err;
