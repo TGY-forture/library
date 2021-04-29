ab9d498197e8f815c2bee431a7c50a27f075bc31
TGY-forture <1981472184@qq.com>
2021-04-29 22:56:47 +0800

    config axios

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import App from './src/view/main.vue'
 import './src/assets/css/index.less'
+import axios from './src/assets/js/axios.js';
 
 import { Form, Input, InputNumber, Button, Radio, Modal, Drawer, Select } from 'ant-design-vue';
 
 const app = createApp(App);
+app.config.globalProperties.$axios = axios;
 app.use(Form).use(Input).use(Button).use(Radio).use(Modal).use(Drawer);
 app.use(InputNumber).use(Select)
 app.mount('#app');
