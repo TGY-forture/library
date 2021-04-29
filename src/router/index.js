f77d302377d635eb3fc3967edb83f4722d9c9990
TGY-forture <1981472184@qq.com>
2021-04-29 22:53:52 +0800

    clean code

--- src/router/index.js
+++ src/router/index.js
@@ -1,37 +1,50 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from "vue-router";
 
-const seat = () => import('../components/seat/select.vue');
-const profile = () => import('../components/profile/profile.vue');
-const appoint = () => import('../components/appoint/appoint.vue');
-const message = () => import('../components/message/message.vue');
+const seat = () => import("../components/seat/select.vue");
+const profile = () => import("../components/profile/profile.vue");
+const appoint = () => import("../components/appoint/appoint.vue");
+const message = () => import("../components/message/message.vue");
+const chat = () => import("../components/message/chat.vue");
+const search = () => import('../components/search/search.vue')
 
 const routes = [
-  { path: '', component: seat },
+  { path: "/redirect.html", component: seat },
+  { path: "/", component: seat },
   {
-    name: 'seat',
-    path: '/seat',
-    component: seat
+    name: "seat",
+    path: "/seat",
+    component: seat,
   },
   {
-    name: 'profile',
-    path: '/profile',
-    component: profile
+    name: "profile",
+    path: "/profile",
+    component: profile,
   },
   {
-    name: 'appoint',
-    path: '/appoint',
-    component: appoint
+    name: "appoint",
+    path: "/appoint",
+    component: appoint,
   },
   {
-    name: 'message',
-    path: '/message',
-    component: message
+    name: "message",
+    path: "/message",
+    component: message,
+  },
+  {
+    name: "chat",
+    path: "/chat",
+    component: chat,
+  },
+  {
+    name: 'search',
+    path: '/search',
+    component: search
   }
 ];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
-})
+  routes,
+});
 
 export default router;
