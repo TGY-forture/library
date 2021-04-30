f5b9e97e64d4fc120e030da5159912b13262ad93
TGY-forture <1981472184@qq.com>
2021-04-30 23:03:41 +0800

    use axios interceptor

--- src/assets/js/axios.js
+++ src/assets/js/axios.js
@@ -3,4 +3,30 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://10.136.21.90:5000';
 axios.defaults.withCredentials = true;
 
+axios.interceptors.request.use((config) => {
+  const accessToken = localStorage.getItem('accessToken');
+  const refreshToken = localStorage.getItem('refreshToken')
+  if (refreshToken && accessToken) {
+    config.headers.Authorization = 'Bearer ' + accessToken;
+    config.headers.RefreshToken = refreshToken;
+  }
+  return config;
+});
+
+axios.interceptors.response.use((response) => {
+  const { accessToken, refreshToken } = response.data;
+  const { location } = response.headers;
+  if (location !== undefined) {
+    window.location.href = location;
+    return response;
+  }
+  if (accessToken) {
+    localStorage.setItem('accessToken', accessToken);
+  }
+  if (refreshToken) {
+    localStorage.setItem('refreshToken', refreshToken);
+  }
+  return response;
+})
+
 export default axios;
