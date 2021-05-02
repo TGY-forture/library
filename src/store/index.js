50a51dc273301eccd4f50be3b1a9cddecb2b6890
TGY-forture <1981472184@qq.com>
2021-05-02 23:25:33 +0800

    add 'inituserinfo' actions for vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,37 @@
 import { createStore } from 'vuex';
+import axios from '../assets/js/axios.js';
 
 let store = createStore({
-  state: {}
+  state() {
+    return {
+      user: {
+        age: 18,
+        avatar: '',
+        email: '',
+        faculty: '',
+        nickname: '暂无',
+        star: 0,
+        gender: 'man',
+        student: '',
+        github: '0',
+        phone: ''
+      }
+    }
+  },
+  getters: {},
+  mutations: {
+    initUserInfo(state, data) {
+      state.user = data;
+      console.log(data);
+    }
+  },
+  actions: {
+    async getUserInfo({ commit }) {
+      const { data } = await axios.get('/user');
+      commit('initUserInfo', data)
+    }
+  },
+  modules: []
 })
 
 export default store;
