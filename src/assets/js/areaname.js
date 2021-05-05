3f8ff9b419a8fe5dca575cb98500176757073d51
TGY-forture <1981472184@qq.com>
2021-05-05 23:22:39 +0800

    add name map

---
+++ src/assets/js/areaname.js
@@ -0,0 +1,16 @@
+export const seatArea = {
+  hall: '大厅',
+  corridor: '走廊',
+  studyroom: '自习室(249南)',
+  social_1: '社科1区(310南)',
+  social_2n: '社科2区(305北)',
+  social_2w: '社科2区(304西)',
+  social_3w: '社科3区(404西)',
+  social_3n: '社科3区(405北)',
+  social_4s: '社科4区(410南)',
+  social_4e: '社科4区(409东)',
+  referencebook: '工具书区(510南)',
+  nature_1: '自科1区(504西)',
+  nature_2: '自科2区(505北)',
+  english: '外文区(509东)'
+}
