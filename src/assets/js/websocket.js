bf2c6f1ba4993a432c8b06ff163365dc709b1397
TGY-forture <1981472184@qq.com>
2021-05-07 23:14:08 +0800

    test socket  connection(client)

--- src/assets/js/websocket.js
+++ src/assets/js/websocket.js
@@ -7,5 +7,23 @@
 import { io } from 'socket.io-client';
 
 export const createSocket = () => {
-  io('https://10.136.21.90:3000')
+  const socket = io('wss://10.136.21.90:3000', {
+    transports: ["websocket"],
+    // reconnection: false,
+    auth: {
+      accessToken: localStorage.getItem('accessToken')//携带用户token
+    }
+  });
+  socket.on('connect', () => {
+    console.log(socket.id + '已连接',);
+  });
+  socket.on('disconnect', () => {
+    console.log('出现重复连接,已拒绝');
+  });
+  socket.io.on('reconnect', () => {
+    console.log('重新连接成功');
+  })
+  socket.io.on("reconnect_attempt", () => {
+    console.log('尝试重新连接');
+  })
 }
