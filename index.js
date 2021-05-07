8186e7dddf8e344d6136808e557836673e0630d8
TGY-forture <1981472184@qq.com>
2021-05-07 23:09:07 +0800

    add component

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ import './src/assets/css/index.less'
 import { Input, InputNumber, Radio, Switch, Form, Select } from 'ant-design-vue'
 import { DatePicker, TimePicker, Tooltip, Row, Col } from 'ant-design-vue';
 import { Button, Tabs, Upload, Drawer, Pagination, Popconfirm } from 'ant-design-vue';
-import { Modal, Empty, Popover } from 'ant-design-vue';
+import { Modal, Empty, Popover, Tag } from 'ant-design-vue';
 
 const app = createApp(App).use(Router).use(Store);
 app.config.globalProperties.$axios = axios;
 app.use(Input).use(Radio).use(Switch).use(Form).use(InputNumber).use(Select);
 app.use(DatePicker).use(TimePicker).use(Tooltip).use(Row).use(Col).use(Button);
 app.use(Tabs).use(Upload).use(Drawer).use(Pagination).use(Popconfirm).use(Modal);
-app.use(Empty).use(Popover)
-app.mount('#app')
+app.use(Empty).use(Popover).use(Tag);
+app.mount('#app');
