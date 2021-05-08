13b9904fadbb4179795a694cc9a1555d0ffd83a9
TGY-forture <1981472184@qq.com>
2021-05-08 22:58:27 +0800

    change css style,test data

--- src/components/appoint/pending.vue
+++ src/components/appoint/pending.vue
@@ -1,58 +1,105 @@
 <template>
   <div class="u-pending">
     <ul>
-      <li v-for="i in 2" :key="i">
+      <li v-for="(item, index) in orders.pendingorder" :key="index">
         <a-row>
-          <a-col :span="5">状态：</a-col>
-          <a-col>已预约</a-col>
+          <a-col :span="span">状态:</a-col>
+          <a-col>{{ item.status === "1" ? "已预约" : "已签到" }}</a-col>
         </a-row>
         <a-row>
-          <a-col :span="5">日期：</a-col>
-          <a-col>2019-02-13 23:98:01</a-col>
+          <a-col :span="span">楼层:</a-col>
+          <a-col>{{ item.floor }}</a-col>
         </a-row>
         <a-row>
-          <a-col :span="5">时间段：</a-col>
-          <a-col>已预约</a-col>
+          <a-col :span="span">区域:</a-col>
+          <a-col>{{ genArea(item.area) }}</a-col>
         </a-row>
         <a-row>
-          <a-col :span="24">
-            <a-button type="primary" @click="hitCard">签到</a-button>
-            <a-button type="dashed">签退</a-button>
-            <a-popconfirm title="确定取消吗?" cancelText="No" okText="Ok">
-              <template #icon>
-                <question-circle-outlined style="color: red" />
-              </template>
-              <a-button type="danger">取消</a-button>
-            </a-popconfirm>
+          <a-col :span="span">座位号:</a-col>
+          <a-col>{{ item.seq }}</a-col>
+        </a-row>
+        <a-row>
+          <a-col :span="span">开始时间:</a-col>
+          <a-col>
+            <a-tag>{{ item.date + " " + genTime(item.start) }}</a-tag>
           </a-col>
+        </a-row>
+        <a-row>
+          <a-col :span="span">结束时间:</a-col>
+          <a-col>
+            <a-tag>{{ item.date + " " + genTime(item.end) }}</a-tag>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-button
+            type="primary"
+            @click="signIn(item)"
+            :disabled="item.status === '2'"
+            >签到</a-button
+          >
+          <a-button
+            type="dashed"
+            @click="signOut(item)"
+            :disabled="item.status === '1'"
+            >签退</a-button
+          >
+          <a-button
+            type="danger"
+            @click="cancel(item)"
+            :disabled="item.status === '2'"
+            >取消</a-button
+          >
         </a-row>
       </li>
     </ul>
-    <Scan :visible="visible" @hideModal="visible = false" />
+    <Scan :visible="visible" @hideModal="visible = false" :info="curorder" />
   </div>
 </template>
 
 <script>
-import { defineComponent, ref } from "vue";
-import { QuestionCircleOutlined } from "@ant-design/icons-vue";
+import { defineComponent } from "vue";
 import Scan from "../util/qrcode.vue";
+import { mapState } from 'vuex';
+import moment from 'moment';
+import { seatArea } from '@/assets/js/areaname.js';
+import { Modal } from 'ant-design-vue';
 
 export default defineComponent({
   name: "pending",
   components: {
-    QuestionCircleOutlined,
     Scan,
   },
-  setup() {
-    let visible = ref(false);
-    const hitCard = () => {
-      visible.value = true;
-    };
+  data() {
     return {
-      visible,
-      hitCard,
-    };
+      visible: false,
+      curorder: {},
+      span: 10
+    }
   },
+  computed: {
+    ...mapState(['orders'])
+  },
+  methods: {
+    genTime(time) {
+      return moment(new Date(+time).toISOString()).format('HH:mm')
+    },
+    signIn(item) {
+      this.curorder = Object.assign(item, { isin: true }); //isin代表是否签到操作
+      this.visible = true;
+    },
+    signOut(item) {
+      this.curorder = Object.assign(item, { isin: false });
+      this.visible = true;
+    },
+    genArea(area) {
+      return seatArea[area];
+    },
+    cancel(item) {
+      Modal.confirm({
+        content: '你确定要取消吗?'
+      })
+    }
+  }
 });
 </script>
 
@@ -63,13 +110,11 @@ export default defineComponent({
     border-left: 4px solid rgba(20, 150, 236, 0.74);
     margin-bottom: 10px;
     .ant-row {
-      margin-top: 8px;
-      &:nth-child(4) {
-        .ant-col {
-          text-align: center;
-          .ant-btn-dashed {
-            margin: 0 20px;
-          }
+      margin-top: 12px;
+      &:last-child {
+        justify-content: center;
+        .ant-btn-dashed {
+          margin: 0 20px;
         }
       }
     }
