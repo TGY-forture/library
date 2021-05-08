c84ea38b07ab77d44ea3db818e3d2c6c942a3cba
TGY-forture <1981472184@qq.com>
2021-05-08 22:59:33 +0800

    step1: show history data

--- src/components/appoint/history.vue
+++ src/components/appoint/history.vue
@@ -1,24 +1,38 @@
 <template>
   <div class="u-history">
     <ul class="history-lists">
-      <li v-for="i in 10" :key="i">
+      <li
+        v-for="(item, index) in showOrder"
+        :key="index"
+        :style="{ backgroundColor: background(item.status) }"
+      >
         <a-row>
-          <a-col :span="5">状态：</a-col>
-          <a-col>已预约</a-col>
+          <a-col :span="7">状态：</a-col>
+          <a-col>{{ statusText(item.status) }}</a-col>
         </a-row>
         <a-row>
-          <a-col :span="5">日期：</a-col>
-          <a-col>2019-02-13 23:98:01</a-col>
+          <a-col :span="7">楼层:</a-col>
+          <a-col>{{ item.floor }}</a-col>
         </a-row>
         <a-row>
-          <a-col :span="5">时间段：</a-col>
-          <a-col>已预约</a-col>
+          <a-col :span="7">区域:</a-col>
+          <a-col>{{ tran(item.area) }}</a-col>
+        </a-row>
+        <a-row>
+          <a-col :span="7">座位号:</a-col>
+          <a-col>{{ item.seq }}</a-col>
+        </a-row>
+        <a-row>
+          <a-col :span="24">
+            <a-tag>{{ item.date + " " + timeTrans(item.start) }}</a-tag>
+            <a-tag>{{ item.date + " " + timeTrans(item.end) }}</a-tag>
+          </a-col>
         </a-row>
       </li>
     </ul>
     <a-pagination
       v-model:current="page"
-      :total="200"
+      :total="orders.pendingorder.length"
       :pageSize="10"
       size="small"
     ></a-pagination>
@@ -26,16 +40,45 @@
 </template>
 
 <script>
-import { defineComponent, ref } from "vue";
+import { defineComponent } from "vue";
+import { mapState } from 'vuex';
+import moment from 'moment';
+import { seatArea } from '@/assets/js/areaname.js';
 
 export default defineComponent({
   name: "history",
-  setup() {
-    let page = ref(3);
+  data() {
     return {
-      page,
-    };
+      page: 1,
+    }
   },
+  computed: {
+    ...mapState(['orders']),
+    showOrder() {
+      return this.orders.pendingorder.slice((this.page - 1) * 10, (this.page - 1) * 10 + 10);
+    }
+  },
+  methods: {
+    statusText(status) {
+      return status;
+    },
+    timeTrans(start) {
+      return moment(new Date(+start)).format('HH:mm');
+    },
+    tran(area) {
+      return seatArea[area];
+    },
+    background(status) {
+      let color;
+      switch (status) {
+        case '3': color = 'rgb(69, 182, 211)'; break; //已签退
+        case '4': color = '#949191'; break;//已取消
+        case '5': color = 'f36b6b'; break;//违约
+        default: color = 'rgb(69, 182, 211)'; break;
+      }
+      return color;
+    }
+  }
 });
 </script>
 
@@ -45,23 +88,18 @@ export default defineComponent({
     height: 420px;
     margin: 20px 0;
     padding: 20px;
-    border-radius: 5px;
-    border: 1px solid rgba(216, 216, 216, 0.623);
     overflow-y: scroll;
     &::-webkit-scrollbar {
       display: none;
     }
     li {
       position: relative;
-      margin: 10px 0;
-      border-radius: 8px;
-      padding: 10px 8px 4px;
+      margin-bottom: 15px;
+      border-radius: 2px;
+      padding: 10px 8px;
       color: white;
-      &:nth-child(2n + 1) {
-        background-color: rgb(69, 182, 211);
-      }
-      &:nth-child(2n) {
-        background-color: rgb(83, 223, 106);
+      .ant-row {
+        margin: 8px 0;
       }
     }
   }
