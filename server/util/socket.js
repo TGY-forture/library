85b58ec90af65e64596e2f1968b6938458589bcd
TGY-forture <1981472184@qq.com>
2021-05-10 23:51:08 +0800

    change socket settings

--- server/util/socket.js
+++ server/util/socket.js
@@ -14,35 +14,32 @@ const student = new Map();//存储已经建立的连接
 wsServer.use(async (socket, next) => {
   let { accessToken } = socket.handshake.auth;
   let { user } = jwt.verify(accessToken, fs.readFileSync('../public/san_domain_com.key'), { algorithm: 'HS256' });
-  if (student.has(user)) { //如果同意浏览器开多个相同页面将不允许再次建立新的连接
-    socket.disconnect(); //关闭重复的连接
-  } else {
-    student.set(user, socket.id);
-    next();
+  if (student.has(user)) { //如果同一浏览器开多个相同页面将不允许再次建立新的连接
+    let prev = student.get(user);
+    if (prev.connected) {
+      prev.disconnect();//关闭之前同一用户的连接
+    }
   }
+  student.set(user, socket);
+  next();
 });
 
 wsServer.on("connection", async (socket) => {
-  //预定座位事件
-  socket.on('book', () => {
-
-  });
-  //签到事件
-  socket.on('signin', () => {
-
-  })
-  //签退事件
-  socket.on('signout', () => {
-
-  })
-  //取消事件
-  socket.on('cancel', () => {
-
-  })
-  //违约事件
-  socket.on('violation', () => {
-
+  console.log(socket.rooms);
+  socket.on('disconnect', () => {
+    console.log('This scoket has been closed.')
   })
 });
 
-httpsServer.listen(3000, '10.136.21.90')
+setInterval(() => {
+  wsServer.fetchSockets().then((sockets) => {
+    let res = [];
+    for (let socket of sockets) {
+      res.push(socket.id);
+    }
+    console.log(...res);
+
+  })
+}, 5000);
+
+httpsServer.listen(3000, '10.136.21.90');
