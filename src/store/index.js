e4c7ff85056355b65b0c142d317590f541bdd41e
TGY-forture <1981472184@qq.com>
2021-05-11 23:15:37 +0800

    add chat function

--- src/store/index.js
+++ src/store/index.js
@@ -4,9 +4,10 @@ import axios from '../assets/js/axios.js';
 let store = createStore({
   state() {
     return {
+      socket: null,//websocket连接
       user: {
         age: 18,
-        avatar: 'https://10.136.21.90:8080/img/1232381748.jpeg',
+        avatar: '',
         email: '',
         faculty: '',
         nickname: '暂无',
@@ -18,7 +19,9 @@ let store = createStore({
       },
       seat: [],
       record: [],
-      orders: { pendingorder: [], completeorder: [] }
+      orders: { pendingorder: [], completeorder: [] },
+      message: {},//{ 1722: { dis: '0', message: [{ send,recv,time,dis,mess }] } }
+      otherusers: {},//{ 1822: { 用户信息 }, 1722: { 用户信息 } }
     }
   },
   getters: {
@@ -32,8 +35,29 @@ let store = createStore({
         const { seq, id, status, user } = obj;
         areaitem[obj.area].push({ seq, id, status, user });
       });
-      // console.log(result);
       return result;
+    },
+    showMess({ message, otherusers }) {//[{ message: 'xxx', user: { 用户信息 }, dis: '0' }, {}]
+      let ret = [];
+      for (let k in message) {
+        let mes = {};
+        mes.message = message[k].message.sort((a, b) => (+b.time) - (+a.time))[0];
+        mes.user = otherusers[k] ? otherusers[k] : {};
+        mes.dis = message[k].dis;
+        ret.push(mes);
+      }
+      console.log(ret);
+      return ret;
+    },
+    pureMess({ message }) {//形式:{ 1722: [[{ 用户消息}, { 用户消息 }]] }
+      let res = {};
+      for (let user in message) {
+        if (!res[user]) {
+          res[user] = message[user].message;
+        }
+        res[user].sort((a, b) => (+a.time) - (+b.time));
+      }
+      return res;
     }
   },
   mutations: {
@@ -48,6 +72,30 @@ let store = createStore({
     },
     initUserRecord(state, data) {
       state.orders = data;
+    },
+    initUserMessage(state, data) {
+      state.message = data;
+    },
+    initOthers(state, data) {
+      state.otherusers = data;
+    },
+    addUser(state, { value, data }) {
+      if (state.otherusers[value]) return;
+      state.otherusers[value] = data;
+    },
+    updateMess(state, { info, type }) {
+      const { recv, send, dis } = info;
+      let other = type === 'send' ? recv : send;
+      if (!state.message[other]) {
+        state.message[other] = {
+          message: []
+        }
+      }
+      state.message[other].dis = dis;
+      state.message[other].message.push(info);
+    },
+    setSocket(state, newsocket) {
+      state.socket = newsocket;
     }
   },
   actions: {
@@ -80,6 +128,14 @@ let store = createStore({
     async getUserRecord({ commit }) {
       let { data } = await axios.get('/userseat');
       commit('initUserRecord', data);
+    },
+    async getUserMessage({ commit }) {
+      const { data } = await axios.get('/chat');
+      commit('initUserMessage', data);
+      if (Object.keys(data).length > 0) {
+        let { data: otheruser } = await axios.post('/others', Object.keys(data));
+        commit('initOthers', otheruser)
+      }
     }
   },
   modules: []
