30e11490bb83342e8c25748b1c79d862570604bc
TGY-forture <1981472184@qq.com>
2021-05-12 23:00:35 +0800

    set websocket config

--- src/assets/js/websocket.js
+++ src/assets/js/websocket.js
@@ -3,27 +3,36 @@
  * 1.及时更新座位信息
  * 2.提供聊天功能
  */
-
 import { io } from 'socket.io-client';
+import store from '@/store/index.js';
+
+const eventListener = () => {
+  store.dispatch('getSeatInfo');//获取座位信息
+  store.dispatch('getBookRecord');//获取座位预约记录
+  store.dispatch('getUserRecord');//获取个人预约记录
+}
 
 export const createSocket = () => {
   const socket = io('wss://10.136.21.90:3000', {
     transports: ["websocket"],
-    // reconnection: false,
+    reconnection: false,
     auth: {
       accessToken: localStorage.getItem('accessToken')//携带用户token
     }
   });
+  socket.on('update', eventListener);
+  /*以下为socket连接相关事件*/
   socket.on('connect', () => {
-    console.log(socket.id + '已连接',);
+    console.log(socket.id + ' has connected.',);
   });
-  socket.on('disconnect', () => {
-    console.log('出现重复连接,已拒绝');
+  socket.on('disconnect', (reason) => {
+    console.log('socket disconnected. reason:' + reason);
   });
   socket.io.on('reconnect', () => {
-    console.log('重新连接成功');
+    console.log('reconnect success!');
   })
   socket.io.on("reconnect_attempt", () => {
-    console.log('尝试重新连接');
+    console.log('trying to reconnect.....');
   })
+  return socket;
 }
